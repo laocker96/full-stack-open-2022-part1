3fc7d0f9e87b532109df36f69b9d1b8ac60d4f0d
Simone Bergamin <simone.bergamin2a@gmai.com>
2022-06-25 02:16:17 +0200

    Exercise 1.14

--- anecdotes/src/App.js
+++ anecdotes/src/App.js
@@ -1,39 +1,51 @@
 import { useState } from 'react'
 
+const Anecdote = ({ anecdote }) => {
+  return (
+    <>
+      <p>{anecdote.text}</p>
+      <p>has {anecdote.vote} votes</p>
+    </>
+
+  );
+
+}
+
 const App = () => {
 
   const [anecdotes, setAnecdotes] = useState([
     {
       "text": 'If it hurts, do it more often',
-      "votes": 0
+      "vote": 0
 
     },
     {
       "text": 'Adding manpower to a late software project makes it later!',
-      "votes": 0
+      "vote": 0
     },
     {
       "text": 'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-      "votes": 0
+      "vote": 0
     },
     {
       "text": 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-      "votes": 0
+      "vote": 0
     },
     {
       "text": 'Premature optimization is the root of all evil.',
-      "votes": 0
+      "vote": 0
     },
     {
       "text": 'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-      "votes": 0
+      "vote": 0
     },
     {
       "text": 'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients',
-      "votes": 0
+      "vote": 0
     }
   ])
   const [selected, setSelected] = useState(0)
+  const [mostVoted, setMostVoted] = useState(0);
 
   const handleClick = () => {
     let randomNumber = Math.floor(Math.random() * anecdotes.length);
@@ -41,17 +53,25 @@ const App = () => {
   }
 
   const handleVote = () => {
+    //handles 
     const copy = [...anecdotes];
-    copy[selected].votes++;
+    copy[selected].vote++;
     setAnecdotes(copy);
+
+    // handle most voted
+    const votes = copy.map(anecdote => anecdote.vote);
+    const maxVote = Math.max(...votes);
+    setMostVoted(votes.indexOf(maxVote));
   }
 
   return (
     <div>
-      <p>{anecdotes[selected].text}</p>
-      <p>has {anecdotes[selected].votes} votes</p>
+      <h1>Anecdote of the day</h1>
+      <Anecdote anecdote={anecdotes[selected]} />
       <button onClick={handleVote}>vote</button><button onClick={handleClick}>next anecdote</button>
-    </div>
+      <h1>Anecdote with most votes</h1>
+      <Anecdote anecdote={anecdotes[mostVoted]} />
+    </div >
   )
 }
 
